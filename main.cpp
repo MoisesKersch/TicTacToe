4b7c1a19c20729b90cca2f4f9fe3087659587f33
MoisesKersch <34980935+MoisesKersch@users.noreply.github.com>
2017-12-31 17:41:34 -0200

    my second commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
+char canvas[3][3] = {{'0','0','0'},
+                     {'1','1','1'},
+                     {'2','2','2'}};
+
+
+void draw()
+{
+    for (int x=0; x<3; x++)
+    {
+        for (int y=0; y<3; y++)
+        {
+            cout << canvas[x][y];
+
+        }
+        cout << "\n";
+    }
+
+}
+
 int main()
 {
-    cout << "" << endl;
-    return 0;
 }
