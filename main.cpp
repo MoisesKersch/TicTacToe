23d4d8c2ea4b1dcc94c8b12abc570c1b7ad23fb2
MoisesKersch <34980935+MoisesKersch@users.noreply.github.com>
2017-12-31 18:32:01 -0200

    updating

--- main.cpp
+++ main.cpp
@@ -1,27 +1,72 @@
 #include <iostream>
+#define cls system ("cls")
 
 using namespace std;
 
 char canvas[3][3] = {{'0','0','0'},
-                     {'1','1','1'},
-                     {'2','2','2'}};
+    {'1','1','1'},
+    {'2','2','2'}
+};
+
+void display();
+void update();
+void play();
+void logic();
 
 
-void draw()
+
+int main()
+{
+
+    play();
+}
+
+void display()
 {
     for (int x=0; x<3; x++)
     {
         for (int y=0; y<3; y++)
         {
             cout << " " << canvas[x][y];
-
         }
         cout << "\n";
     }
 }
 
-int main()
+void update(int x, int y, char input)
 {
-    draw();
+    canvas[x][y] = input;
+}
 
+void logic()
+{
+
+}
+
+void play()
+{
+
+    int x, y;
+
+    int i = 0;
+
+    while (i < 9)
+    {
+        display();
+        cout << "Enter the coordinates: ";
+        cin >> x >> y;
+        if (x < 4 and y < 4 and (canvas[x-1][y-1] != 'X' and  canvas[x-1][y-1] != 'O'))
+        {
+            i++;
+            if ((i % 2) == 0)
+            {
+               update(--x, --y, 'X');
+            }
+            else
+            {
+               update(--x, --y, 'O');
+            }
+        }
+        cls;
+    }
 }
