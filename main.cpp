7b7d947f1fc8b416ded186baecf73681a0713ce6
MoisesKersch <34980935+MoisesKersch@users.noreply.github.com>
2017-12-31 22:14:06 -0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,26 +7,10 @@ char canvas[3][3] = {{'0','0','0'},
                      {'0','0','0'},
                      {'0','0','O'}};
 
-int a = 0;
-int b = 0;
-int c = 0;
-int d = 0;
-int e = 0;
-int f = 0;
-int g = 0;
-int h = 0;
-int i = 0;
-int j = 0;
-int k = 0;
-int l = 0;
-int m = 0;
-int n = 0;
-
 void display();
 void update(int x, int y, char input);
 void play();
 bool logic();
-
 
 int main()
 {
@@ -77,18 +61,14 @@ bool logic()
         {
             return false;
         }
-
     }
-
 }
 
 void play()
 {
-
     int x, y;
-
     int i = 0;
-
+  
     while (i < 9 and !logic())
     {
         display();
