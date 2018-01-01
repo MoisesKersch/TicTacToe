21e66aa3818298c44f42060cf2d0c247dbea6ef2
MoisesKersch <34980935+MoisesKersch@users.noreply.github.com>
2018-01-01 12:01:01 -0200

    TitTacToe

--- main.cpp
+++ main.cpp
@@ -5,12 +5,28 @@ using namespace std;
 
 char canvas[3][3] = {{'0','0','0'},
                      {'0','0','0'},
-                     {'0','0','O'}};
+                     {'0','0','0'}};
+
+int a = 0;
+int b = 0;
+int c = 0;
+int d = 0;
+int e = 0;
+int f = 0;
+int g = 0;
+int h = 0;
+int i = 0;
+int j = 0;
+int k = 0;
+int l = 0;
+int m = 0;
+int n = 0;
 
 void display();
 void update(int x, int y, char input);
 void play();
 bool logic();
+
 
 int main()
 {
@@ -36,44 +52,101 @@ void update(int x, int y, char input)
 
 bool logic()
 {
-    int xcounter, ocounter;
-    for(int x=0; x<3; x++)
+    for (int x=0; x<3; x++)
     {
-        xcounter = 0;
-        ocounter = 0;
-        for(int y=0; y<3; y++)
+        for (int y=0; y<3; y++)
         {
-            if(canvas[x][y] == 'X')
-                xcounter++;
-            if(canvas[x][y] == 'O')
-                ocounter++;
-        }
+            // 1 line
+            if (x == 0 && canvas[x][y] == 'X')
+            {
+                a++;
+                cout << "a: " << a << endl;
+            }
+            if (x == 0 && canvas[x][y] == 'O')
+            {
+                b++;
+            }
+            // 2 line
+            if (x == 1 && canvas[x][y] == 'X')
+            {
+                c++;
+            }
+            if (x == 1 && canvas[x][y] == 'O')
+            {
+                d++;
+            }
+            // 3 line
+            if (x == 2 && canvas[x][y] == 'X')
+            {
+                e++;
+            }
+            if (x == 2 && canvas[x][y] == 'O')
+            {
+                f++;
+            }
 
-        if (xcounter == 3)
-        {
-            return true;
+
+            // 1 column
+            if (y == 0 && canvas[x][y] == 'X')
+            {
+                g++;
+            }
+            if (y == 0 && canvas[x][y] == 'O')
+            {
+                h++;
+            }
+            // 2 column
+            if (y == 1 && canvas[x][y] == 'X')
+            {
+                i++;
+            }
+            if (y == 1 && canvas[x][y] == 'O')
+            {
+                j++;
+            }
+            // 3 column
+            if (y == 2 && canvas[x][y] == 'X')
+            {
+                k++;
+            }
+            if (y == 2 && canvas[x][y] == 'O')
+            {
+                l++;
+            }
+
+            // right line
+            if (x == y && canvas[x][y] == 'X')
+            {
+                m++;
+            }
+            if (x == y && canvas[x][y] == 'O')
+            {
+                n++;
+            }
         }
-        else if (ocounter == 3)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    }
+    //cout << a << b << c << d << e << f << g << h << i << j << k << l << m << n << endl;
+    if (a == 3 || b == 3 || c == 3 || d == 3 || e == 3 || f == 3 || g == 3 || h == 3 || i == 3 || j == 3 || l == 3 || k == 3 || m == 3 || n == 3)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
     }
 }
 
 void play()
 {
+
     int x, y;
+
     int i = 0;
-  
-    while (i < 9 and !logic())
+
+    while (i < 9)
     {
         display();
         cout << "Enter the coordinates: ";
-
         cin >> x >> y;
         if ((x < 4 and x >= 0) and (y < 4 and y >= 0) and (canvas[x-1][y-1] != 'X' and  canvas[x-1][y-1] != 'O'))
         {
@@ -86,7 +159,8 @@ void play()
             {
                 update(--x, --y, 'O');
             }
+            logic();
         }
-        cls;
+        // cls;
     }
 }
